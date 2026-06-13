debf9936d14e93f7bc96d729dcef43ad9a977bbd
Viswas <ambadyviswas7@gmail.com>
2026-06-13 12:04:41 +0530

    Describe your changes

--- boatapp/urls.py
+++ boatapp/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('book',views.bookings,name='book'),
 ]
--- boatapp/views.py
+++ boatapp/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'home.html')
+
+def bookings(request):
+    return render(request,'booking.html')
