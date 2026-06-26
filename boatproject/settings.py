033bd9342e8427ec53611f80a8c4a0d6fa70e786
Viswas <ambadyviswas7@gmail.com>
2026-06-26 11:58:37 +0530

    edit settings

--- boatproject/settings.py
+++ boatproject/settings.py
@@ -136,14 +136,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-
-
-
-
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
 EMAIL_HOST_USER = os.getenv("freshmartshop123@gmail.com")
-EMAIL_HOST_PASSWORD = os.getenv("Houseboat@123<>.")
+EMAIL_HOST_PASSWORD = os.getenv("vdnt xrlx ivzt zoou")
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
